57c6405e7a3f3bdd8cf0354e0341fe307b272cd0
Michael Jiron <hurmi2156@outlook.com>
2020-05-28 18:49:44 -0400

    Write script for tabs display content when clicked

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,5 +12,40 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* This code script executes when the website is first loaded in  */
 
-/*TODO: Will write scripting for tabs so that only certain Sections will show when a specific tab is selected at the top" */
+/* JavaScript "clicks" on the default tab so that content is shown when first going into the website */
+
+window.onload = function( ){
+    displayDefaultSection();
+}
+
+function displayDefaultSection(){
+    document.getElementById("default-tab").click();
+}
+
+/* Displays certain Sections will when a specific tab is selected at the top" */
+function displaySection(event, sectionName){
+    var i, sectionContent, sectionTabs;
+    sectionContent = document.getElementsByClassName("section-content");
+
+    /* Set all content to display = none*/
+    for(i = 0; i < sectionContent.length; i++){
+        sectionContent[i].style.display = "none";
+    }
+
+    sectionTabs = document.getElementsByClassName("section-tab");
+
+    /* Set all tabs from "active" to "" which is changes to default styling for tabs*/
+    for(i = 0; i < sectionTabs.length; i++){
+        sectionTabs[i].className = sectionTabs[i].className.replace(" active", "");
+    }
+    
+    /* Displays the appropriate content for the sectionName associated with the tab the user just clicked on  */
+    document.getElementById(sectionName).style.display = "block";
+
+    /* Changes the styling to active for the tab the user just clicked on */
+    event.currentTarget.className += " active";
+
+}
+
