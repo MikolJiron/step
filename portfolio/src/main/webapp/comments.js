6e00ce19a07f4cf18af8493a9ab5afb46f183de1
Michael Jiron <hurmi2156@outlook.com>
2020-06-06 03:02:41 +0000

    Create class with method to retrieve comments from backend server and then display those comments once retrieved

---
+++ portfolio/src/main/webapp/comments.js
@@ -0,0 +1,60 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Represents a Comments object whose getComments() method retrieves comments data from
+ * the Java servlet and populates the data into a container in the DOM. 
+ */
+class Comments {
+  constructor() {
+    this.commentsListContainer = document.getElementById("comments-container");
+    this.endpointToRetrieveDataFrom = "/comments-data";
+  }
+
+  /**
+   * Fetches a list of comments from a URL via the Java servlet, uses a promise 
+   * to add them to the DOM once the comments have been received.
+   */
+  getComments() {
+    fetch(this.endpointToRetrieveDataFrom)
+      .then(response => response.json())
+      .then((commentsList) => {
+        // Reset the commentsListContainer to reload it with the new list of comments.
+        this.commentsListContainer.innerHTML = '';
+
+        // Add each comment in the JSON to the DOM.
+        commentsList.forEach((comment) => {
+          this.commentsListContainer.appendChild(
+            this.createCommentElement(comment)
+          );
+        });
+      })
+      .catch(() => {
+        console.error("Failed to load comments.");
+      });
+  }
+
+  /** 
+   * Creates an <li> element containing a comment. 
+   * @param {string} text - Text to put into a list element.
+   * @return {*} - Newly created <li> element containing the comment's text.
+   */
+  createCommentElement(text) {
+    const commentElement = document.createElement('li');
+    commentElement.innerText = text;
+    return commentElement;
+  }
+}
+
+export {Comments};
--- portfolio/src/main/webapp/index.js
+++ portfolio/src/main/webapp/index.js
@@ -13,18 +13,25 @@
 // limitations under the License.
 
 "use strict";
+import {Comments} from "./comments.js";
 import {SlideShow} from "./slideshow.js";
 import {Tab} from "./tab.js";
 import {Utils} from "./utils.js";
 
 const tab = new Tab();
 const slideShow = new SlideShow();
+const comments = new Comments();
 
 window.onload = function() {
   // Allow all imported functions to be in the scope of "window" in 
   // window.onload.
   window.tab = tab;
   window.slideShow = slideShow;
+  window.comments = comments;
+
+  // Get the comments from the server and populate them into the 
+  // comments-container.
+  comments.getComments();
 
   // Set up default tab and slideshow.
   tab.displayDefaultSection();
