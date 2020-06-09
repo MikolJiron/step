4784f59527a852e7fabcd0f7b8ad91b9e94196af
MikolJiron <hurmi2156@outlook.com>
2020-06-09 16:25:26 +0000

    Add constants for HTML redirect and JSON content type

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -29,6 +29,8 @@ public class DataServlet extends HttpServlet {
   // TODO: Persist comments using DataStore and noSQL. Issue: DataStore.
   private final ArrayList<String> comments = new ArrayList<String>();
   private final String COMMENT_PARAM = "commentText";
+  private final String HTML_PAGE_TO_REDIRECT_TO = "/index.html";
+  private final String JSON_CONTENT_TYPE = "application/json;";
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -37,7 +39,7 @@ public class DataServlet extends HttpServlet {
     comments.add(newComment);
 
     // Redirect back to the HTML page.
-    response.sendRedirect("/index.html");
+    response.sendRedirect(HTML_PAGE_TO_REDIRECT_TO);
   }
 
   @Override
@@ -46,7 +48,7 @@ public class DataServlet extends HttpServlet {
     String json = convertToJson(comments);
 
     // Send the list of comments as the response.
-    response.setContentType("application/json;");
+    response.setContentType(JSON_CONTENT_TYPE);
     response.getWriter().println(json);
   }
 
