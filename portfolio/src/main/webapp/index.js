294ed2fce93bf5792756c08c48982f8dcfdf9170
Michael Jiron <42727889+MikolJiron@users.noreply.github.com>
2020-06-09 16:21:59 -0400

    Servlet frontend (#42)
    * Implement backend server without data persistence.
    
    * Create class with method to retrieve comments from backend server and then display those comments once retrieved
    
    * Fix discrepancy between frontend and backend field names for the form.
    
    * Fix comment in DataServlet.java
    
    * Change double quotes to single quotes in all JS files
    
    * Add more robust error checking for fetch
    
    * use response.ok() to make sure the response status is correct

--- portfolio/src/main/webapp/index.js
+++ portfolio/src/main/webapp/index.js
@@ -12,22 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-"use strict";
-import {SlideShow} from "./slideshow.js";
-import {Tab} from "./tab.js";
-import {Utils} from "./utils.js";
+'use strict';
+import {Comments} from './comments.js';
+import {SlideShow} from './slideshow.js';
+import {Tab} from './tab.js';
+import {Utils} from './utils.js';
 
 const tab = new Tab();
 const slideShow = new SlideShow();
+const comments = new Comments();
 
 window.onload = function() {
   // Allow all imported functions to be in the scope of "window" in 
   // window.onload.
   window.tab = tab;
   window.slideShow = slideShow;
+  window.comments = comments;
+
+  // Get the comments from the server and populate them into the 
+  // comments-container.
+  comments.getComments();
 
   // Set up default tab and slideshow.
   tab.displayDefaultSection();
-  Utils.setToNone("slide");
+  Utils.setToNone('slide');
   slideShow.showSlide(0);
 }
