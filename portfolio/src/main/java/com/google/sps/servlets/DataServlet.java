409b30841deebf7442ae1e50b0e7e1ea6fff606d
MikolJiron <hurmi2156@outlook.com>
2020-06-09 20:19:23 +0000

    Fix index path

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -29,7 +29,7 @@ public class DataServlet extends HttpServlet {
   // TODO: Persist comments using DataStore and noSQL. Issue: DataStore.
   private final ArrayList<String> comments = new ArrayList<String>();
   private final String COMMENT_PARAM = "commentText";
-  private final String HTML_PAGE_TO_REDIRECT_TO = "/index.html";
+  private final String INDEX_PATH = "/index.html";
   private final String JSON_CONTENT_TYPE = "application/json;";
 
   @Override
@@ -39,7 +39,7 @@ public class DataServlet extends HttpServlet {
     comments.add(newComment);
 
     // Redirect back to the HTML page.
-    response.sendRedirect(HTML_PAGE_TO_REDIRECT_TO);
+    response.sendRedirect(INDEX_PATH);
   }
 
   @Override
