7a3a16c3477f76e74b39a7b42414b0b4cb169033
MikolJiron <hurmi2156@outlook.com>
2020-06-15 14:23:33 +0000

    Add error message with why it occurred

--- portfolio/src/main/webapp/comments.js
+++ portfolio/src/main/webapp/comments.js
@@ -44,7 +44,7 @@ class Comments {
         });
       })
       .catch((error) => {
-        console.log(error);
+        console.log(`${error}. Failed to fetch comments.`);
       });
   }
 
@@ -68,7 +68,7 @@ class Comments {
     fetch(request)
       .then(this.checkPostError)
       .catch((error) => {
-        console.log(error);
+        console.log(`${error}. Error occurred after attempting to delete comments.`);
       });
   }
 
