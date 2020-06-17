149d473afd4255405d43fdd5596c8d220b059f6a
jmj51 <jmj51@duke.edu>
2020-06-17 00:03:14 -0400

    Implement the first part of the algorithm, by getting the all of the TimeRanges in which attendees for my meeting request are occuppied and handle edge cases

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,50 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+  public Collection<TimeRange> query(Collection<Event> scheduledEvents, MeetingRequest request) {
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
+    
+    // First I get the list of all the people that need to attend the meeting request.
+    Set<String> requestedEventAttendees = new HashSet(request.getAttendees());
+
+    // Get the duration of the meeting request.
+    long requestEventDuration = request.getDuration();
+
+    // Handle edge cases.
+    // If the request has no attendees, return WHOLE_DAY.
+    // Else if the duration of the meeting request > WHOLE_DAY, return empty list.
+    // Same thing if the duration is <=0;
+    if(requestedEventAttendees.equals(Collections.emptySet())) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    } else if(requestEventDuration > TimeRange.WHOLE_DAY.duration() || requestEventDuration <= 0) {
+      return Arrays.asList();
+    }
+
+    // Find all of the events containing ANY of the desired attendees.
+
+    // Add any such events' TimeRanges to this list.
+    List<TimeRange> scheduledTimeRanges = new ArrayList<>();
+
+    for(Event event: scheduledEvents) {
+      Set<String> eventAttendees = event.getAttendees();
+      // Get the Set intersection between THIS event's attendees and my requested event's attendees.
+      eventAttendees.retainAll(requestedEventAttendees);
+      if(!eventAttendees.isEmpty()) {
+        scheduledTimeRanges.add(event.getWhen());
+      }
+
+    }
+
+
+    return new HashSet<>();
   }
 }
