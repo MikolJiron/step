4c68ba8af2672b3af2f93c17cad2d618767d5e51
MikolJiron <hurmi2156@outlook.com>
2020-06-18 22:53:34 +0000

    Add back the enum to UserStatus class and delete the AuthState.java file.

--- portfolio/src/main/java/com/google/sps/servlets/AuthenticationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/AuthenticationServlet.java
@@ -41,11 +41,11 @@ public class AuthenticationServlet extends HttpServlet {
     if (userService.isUserLoggedIn()) {
       // The user IS logged in.
       String logoutUrl = userService.createLogoutURL(Params.BASE_URL_PATH);
-      status = new UserStatus(AuthState.isLoggedIn.LOGGED_IN.getState(), logoutUrl);
+      status = new UserStatus(UserStatus.AuthState.LOGGED_IN.getState(), logoutUrl);
     } else {
       // The user IS NOT logged in.
       String loginUrl = userService.createLoginURL(Params.BASE_URL_PATH);
-      status = new UserStatus(AuthState.isLoggedIn.LOGGED_OUT.getState(), loginUrl);
+      status = new UserStatus(UserStatus.AuthState.LOGGED_OUT.getState(), loginUrl);
     }
 
     // Convert the status object to JSON and send the response.
