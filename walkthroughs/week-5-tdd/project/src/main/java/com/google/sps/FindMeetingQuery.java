419efc75f47436d618ffc92e06ed1be7182f5afb
MikolJiron <hurmi2156@outlook.com>
2020-06-21 05:43:46 +0000

    Altered algorithm to work for optional attendees

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -33,25 +33,20 @@ public final class FindMeetingQuery {
    *   available TimeRanges that meet the MeetingRequest criteria. 
    * All desired attendees must be able to attend, and the TimeRanges must be long enough for the 
    *   request to be met.
-   * Overview of algorithm:
-   *   First I need to create a set for all of the people that I want to attend my requested event.
-   *     and get the duration of that event.
-   *   Next, I handle edge cases for when there are no attendees for my request and when the duration
-   *     is out of bounds.
-   *   Now, I make a BitSet that represents all of the minutes in a day. 
-   *   I iterate through each event that has already been scheduled and compare that event's attendee set
-   *     to the set of attendees for my requested event. 
-   *   If there is any intersection between the two sets, that means one of my requested attendees is booked for
-   *     that event.
-   *   I then add that event's TimeRange to my BitSet by setting all of the bits from the TimeRange.start() to
-   *     TimeRange.end() to 1.
-   *   As I add new TimeRanges, it doesn't matter if events overlap since a bit (minute) will already be set to 1
-   *     if at LEAST one attendee is booked during that event.
-   *   Once I'm done, I will have all of the TimeRanges in which a person is booked represented in my BitSet.
-   *   To know all of the TimeRanges in which a person IS NOT booked, I simply flip all of the bits in my BitSet.
-   *   Now I convert the inverted BitSet back to TimeRanges and only add them to my list of TimeRanges to return,
-   *     if they are at least as long as the requested event's duration.git s
-   *   Once I'm done, I will have all of the TimeRanges I need.
+   * Overview of algorithm: 
+   *   I get sets of representing all of the mandatory and optional attendees.
+   *   I also get the duration of the meeting request.
+   *   I calculate all of the minutes in which mandatory attendees are not booked, represented
+   *     by a BitSet.
+   *   I then calculate all of the minutes in which optional attendees are not booked,
+   *     represented by a BitSet.
+   *   Next, I calculate all of the minutes in which BOTH mandatory and optional 
+   *     attendees are not booked, again, represented by a BitSet.
+   *   Now I convert this BitSet representing BOTH back to a List of TimeRanges.
+   *   If there are TimeRanges in which BOTH mandatory and optional attendees can attend,
+   *     I return this List.
+   *   Otherwise, I return the List of TimeRanges in which just mandatory attendees can attend.
+   *
    * @param scheduledEvents - The events that have already been scheduled for the day.
    * @param request - The MeetingRequest that contains all of the desired mandatory attendees 
    *   and desired event length for that request.
@@ -60,20 +55,52 @@ public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> scheduledEvents, MeetingRequest request) {
     // First I get the list of all the people that need to attend the meeting request.
     Set<String> requestedEventAttendees = new HashSet(request.getAttendees());
+    Set<String> requestedEventOptionalAttendees = new HashSet(request.getOptionalAttendees());
 
     // Get the duration of the meeting request.
     long requestEventDuration = request.getDuration();
 
-    // Handle edge cases.
-    // If the request has no attendees, return WHOLE_DAY.
-    // Else if the duration of the meeting request > WHOLE_DAY, return empty list.
-    // Same thing if the duration is <=0;
-    if(requestedEventAttendees.equals(Collections.emptySet())) {
-      return Arrays.asList(TimeRange.WHOLE_DAY);
-    } else if(requestEventDuration > TimeRange.WHOLE_DAY.duration() || requestEventDuration <= 0) {
-      return new ArrayList<>();
+    // Get all of the minutes in which mandatory attendees are not booked.
+    BitSet minutesInDayNotBooked = createBitSetOfNonBookedTimes(scheduledEvents, requestedEventAttendees);
+
+    // Get all of the minutes in which optional attendees are not booked.
+    BitSet minutesInDayNotBookedOptional = createBitSetOfNonBookedTimes(scheduledEvents, requestedEventOptionalAttendees);
+
+    // Get all of the minutes in which both mandatory AND optional attendees are not booked.
+    BitSet minutesInDayNotBookedForBoth = (BitSet) minutesInDayNotBooked.clone();
+    minutesInDayNotBookedForBoth.and(minutesInDayNotBookedOptional);
+
+    // Now we create all of the TimeRanges based off the BitSet for which both mandatory 
+    // and optional attendees are able to attend. 
+    List<TimeRange> timeRangesNotBookedForBoth = convertBitSetToTimeRanges(requestEventDuration, minutesInDayNotBookedForBoth);
+
+    // If there exist any TimeRanges in which both optional and mandatory attendees can attend, 
+    // then return those TimeRanges.
+    if(timeRangesNotBookedForBoth.size() > 0) {
+      return timeRangesNotBookedForBoth;
+    } else if(requestedEventOptionalAttendees.size() > 0 && requestedEventAttendees.size() == 0) {
+      // Handle edge case when there is at least one optional attendee, but no mandatory attendees.
+      // If this is the case, then no TimeRanges should be available.
+      return new ArrayList<TimeRange>();
     }
 
+    // If there are no TimeRanges in which both optional and mandatory attendees can attend,
+    // then simply return the TimeRanges in which JUST mandatory attendees can attend.
+    return convertBitSetToTimeRanges(requestEventDuration, minutesInDayNotBooked);
+  }
+
+  /**
+   * This helper method returns a BitSet representing all of the TimeRanges in which a set of
+   * requestedEventAttendees is not-booked.
+   *   Overview of Procedure:
+   *   Iterate through each Event in my Collection of events and add their TimeRanges to the BitSet
+   *   if and only if the event contains at least one attendee from my Set of requested attendees.
+   * @param scheduledEvents - The events that have already been scheduled for the day.
+   * @param requestedEventAttendees - List of attendees that I want to see when they're free.
+   * @return - The BitSet representing all of the TimeRanges in which a set of
+   *   requestedEventAttendees is not-booked.
+   */
+  public static BitSet createBitSetOfNonBookedTimes(Collection<Event> scheduledEvents, Set<String> requestedEventAttendees) {
     // This BitSet, whose bits represent all of the minutes in a day, will be filled in so
     // that all minutes that are already booked by a desired attendee are set to 1.
     BitSet minutesInDayBooked = new BitSet(TimeRange.END_OF_DAY + 1);
@@ -100,17 +127,30 @@ public final class FindMeetingQuery {
     BitSet minutesInDayNotBooked = (BitSet) minutesInDayBooked.clone();
     minutesInDayNotBooked.flip(0, TimeRange.END_OF_DAY);
 
-    // Now we create all of the TimeRanges based off the BitSet containing all "free" not-booked minutes.
-    List<TimeRange> timeRangesNotBooked = new ArrayList<TimeRange>();
+    return minutesInDayNotBooked;
+  }
+
+  /**
+   * This helper method returns a List of TimeRanges that are at least as long as the
+   * requestedEventDuration, converted from the BitSet representing the 
+   * minutes in a day in which an event occurs.
+   * @param requestedEventDuration - The duration for which needs to be the minimum required event duration.
+   * @param minutesInDay - A BitSet representing the minutes when events in a day occur or will occur.
+   */
+  public static List<TimeRange> convertBitSetToTimeRanges(long requestEventDuration, BitSet minutesInDay) {
+    // We create all of the TimeRanges based off the BitSet.
+    List<TimeRange> timeRanges = new ArrayList<TimeRange>();
     int index = 0;
+
+    // We iterate through the BitSet until there are no more TimeRanges left to create.
     while(index < TimeRange.END_OF_DAY + 1) {
       // Get the start and end times of the TimeRange I want to add to my list.
-      int newStartTime = minutesInDayNotBooked.nextSetBit(index);
-      int newEndTime = minutesInDayNotBooked.nextClearBit(index) - 1;
+      int newStartTime = minutesInDay.nextSetBit(index);
+      int newEndTime = minutesInDay.nextClearBit(index) - 1;
 
       // Whenever there are no more clear bits = 0, we set the endTime to END_OF_DAY since every minute
       // afterwards is now available to create this new event.
-      if(minutesInDayNotBooked.nextClearBit(index) == TimeRange.END_OF_DAY) {
+      if(minutesInDay.nextClearBit(index) == TimeRange.END_OF_DAY) {
         newEndTime = TimeRange.END_OF_DAY;
       }
       // Now, we should create the new TimeRange.
@@ -118,12 +158,12 @@ public final class FindMeetingQuery {
       
       // We must check if the TimeRange is long enough, if so, then we add to the list.
       if(nonBookedTime.duration() >= requestEventDuration) {
-        timeRangesNotBooked.add(nonBookedTime);
+        timeRanges.add(nonBookedTime);
       }
       
       // Our index that we should look for to find the next set of bits should be
       // the nextSetBit after my last setBit.
-      index = minutesInDayNotBooked.nextSetBit(newEndTime + 1);
+      index = minutesInDay.nextSetBit(newEndTime + 1);
 
       // If there are no more set bits = 1, that means we've reached the end of available times
       // and we should break out of the loop.
@@ -132,6 +172,6 @@ public final class FindMeetingQuery {
       }
     }
 
-    return timeRangesNotBooked;
+    return timeRanges;
   }
 }
